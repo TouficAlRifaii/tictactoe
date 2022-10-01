d77d4f2e0e38b984e357f128e943b0ee16f76942
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-10-01 13:27:54 +0300

    imgs handling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,40 @@
 window.onload= () => {
-    const locations = document.querySelectorAll(".location")
-    locations[0].innerHTML= "<img src='assests/yellow.png' />"
+    const locations = document.getElementsByClassName("location")
+    let isPlayer = 1; // This variable will determine if it is the player turn (1== player)
+    // locations[1].innerHTML = "<img src='assests/red.png'>"
+    function turn(event, index){
+        console.log(index)
+        if(isPlayer){
+            event.innerHTML = "<img src='assests/red.png'>";
+            event.classList.add("afterimg");
+            if(index>=3 && index<=5){
+                console.log("inside first if")
+                event.classList.add("afterImgrow2");
+            }
+            else if(index>=6){
+                console.log("inside second if");
+                event.classList.add("afterImgrow3");
+            }
+            isPlayer = 0;
+        }
+        else{
+            event.innerHTML = "<img src='assests/yellow.png'>";
+            event.classList.add("afterimg");
+            if(index>=3 && index<=5){
+                console.log("inside first if")
+                event.classList.add("afterImgrow2");
+            }
+            else if(index>=6){
+                console.log("inside second if");
+                event.classList.add("afterImgrow3");
+            }
+            isPlayer = 1;
+        }
+
+    }
+    for(let i = 0; i<locations.length; i++){
+        
+        locations[i].addEventListener("click" , ()=> turn(locations[i] , i));
+    }
     
 };
