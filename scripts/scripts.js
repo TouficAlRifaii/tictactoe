4815a85ed615ac1413385bb73df3f726b55cea36
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-10-01 19:37:54 +0300

    adding variables to get the current  game Board

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,53 @@
 window.onload = () => {
   const locations = document.getElementsByClassName("location");
-  let isPlayer = 1; // This variable will determine if it is the player turn (1== player)
+  const win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  let gameState = 1; // 1 if game is on, 0 if the game has ended;
+  let comparisonResult;
+  const gameBoard = [
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+    "empty",
+  ];
+  let isPlayer = 1; // This variable will determine if it is the player turn (1 == player)
   function turn(event, index) {
     if (event.innerHTML == "") {
       if (isPlayer) {
         event.innerHTML = "<img src='assests/red.png'>";
         event.classList.add("afterimg");
+        gameBoard[index] = "x";
         if (index >= 3 && index <= 5) {
-          console.log("inside first if");
           event.classList.add("afterImgrow2");
         } else if (index >= 6) {
-          console.log("inside second if");
           event.classList.add("afterImgrow3");
         }
         isPlayer = 0;
+        console.log(gameBoard.toString());
       } else {
         event.innerHTML = "<img src='assests/yellow.png'>";
         event.classList.add("afterimg");
+        gameBoard[index]="o";
         if (index >= 3 && index <= 5) {
-          console.log("inside first if");
           event.classList.add("afterImgrow2");
         } else if (index >= 6) {
-          console.log("inside second if");
           event.classList.add("afterImgrow3");
         }
         isPlayer = 1;
+        console.log(gameBoard.toString());
       }
     }
   }
